e8a26fad92b1a3163af7b11c2436a09e1ee4cbfd
Debra Gordon <debra.m.gordon@gmail.com>
2017-01-12 19:01:06 -0600

    not sure how to start

--- javascripts/mainController.js
+++ javascripts/mainController.js
@@ -1,16 +1,29 @@
 ;(function () {
   angular.module('fetchApp').controller('MainController', function ($scope, requestService, Response) {
     $scope.responses = [];
-    $scope.url = "http://httpstat.us/200";
+    $scope.url ;
 
-    $scope.fetch = () => {
-      requestService.get($scope.url).then((success) => {
-        const response = new Response(success);
-        $scope.responses.push(response);
-      }, (error) => {
-        debugger
-        //do something else
-      });
-    };
+    // user enters URL - bind their response as the response.url
+    // user selects type of response - bind their selection as the response.method
+    // user clicks submit
+      // based on the response.method, the url they entered is contacted and data is obtained
+      // run through a switch statement to determine with method to use
+        // GET
+        // POST
+        // OPTIONS
+        // HEAD
+      // INFO received from URL is then displayed in the "results" div
   });
 })();
+
+
+  // $scope.fetch = () => {
+
+  //   requestService.get($scope.url).then((success) => {
+  //       const response = new Response(success);
+  //       $scope.responses.push(response);
+  //   }, (error) => {
+  //     debugger
+  //     //do something else
+  //   });
+  // };
